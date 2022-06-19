5f80260705a8c711f6c980584cfe4b1e992ef50f
SM-21 <sm@ayoubcomputers.com>
2022-06-19 23:05:31 +0300

    2206191105

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: 
+  {
+    extend: 
+    {
+      colors:
+      {
+        "a-primary": "#2c627e",
+        "a-secondary": "#859eaa",
+        "a-tertiary": "#549aad" /* #54a094 #7ca094  */
+      }
+    },
+  },
+  plugins: [],
+}
